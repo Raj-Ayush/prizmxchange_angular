d483253fb779acc14113cb7424ac6a1ebfccd7a7
Raj-Ayush <rajayush8571@gmail.com>
2024-02-25 18:25:00 +0530

    fix(files):html static data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FootersComponent } from './components/footers/footers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, DashboardComponent, FootersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WhatisprizmComponent } from './components/whatisprizm/whatisprizm.component';
+import { ParaMiningComponent } from './components/para-mining/para-mining.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: DashboardComponent },
+    { path: 'how_prizmx', component: WhatisprizmComponent},
+    { path: 'paramining_content', component: ParaMiningComponent},
+];
